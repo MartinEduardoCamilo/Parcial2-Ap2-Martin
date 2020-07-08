097861ad36540d20f8cf876d1fef3d2917b674df
Martin Eduardo Camilo De La Cruz <martin_camilo@ucne.edu.do>
2020-07-07 20:55:32 -0400

    add file

---
+++ SegundoParcial/DAL/Contexto.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SegundoParcial.DAL
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=DATA\parcial2.db");
+        }
+
+
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<>().HasData(new 
+        //    {
+                
+        //    });
+
+        //}
+    }
